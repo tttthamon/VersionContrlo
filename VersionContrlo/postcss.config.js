3f4e16ab9ca5d3bffb26e02d35b0d6e5812fe8d4
tttthamon <thamonnuanmuak@gmail.com>
2025-08-27 14:01:47 +0700

    Revert "Reset master"
    This reverts commit ee812fb767ec25f467ebf69fcb1ed5443fca7c4f.

--- VersionContrlo/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-import autoprefixer from 'autoprefixer'
-
-export default {
-  plugins: [
-    // to edit target browsers: use "browserslist" field in package.json
-    autoprefixer
-  ]
-}
